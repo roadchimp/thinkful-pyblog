91c47d62c4fb2fb6cc5dfc67124de1f975425ec1
Sam Sena <samsena@gmail.com>
2015-03-25 23:35:58 +0000

    First push

---
+++ blog/__init__.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
+# blog.config refers to the path \blog\config
+app.config.from_object(config_path)
+
+from . import views
+from . import filters
